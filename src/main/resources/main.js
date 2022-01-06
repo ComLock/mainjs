caafdc8e18e73f66ccda646267c1308fc63d9a19
Christian Westgaard <cwe@enonic.com>
2022-01-06 10:05:59 +0100

    Commit before moving to new pc

--- src/main/resources/main.js
+++ src/main/resources/main.js
@@ -1,5 +1,7 @@
-const libCluster = require '/lib/xp/cluster';
-log.info(`Starting ${app.name} ${app.version} isMaster:${libCluster.isMaster()} config:${JSON.stringify(app.config, null, 4)}`);
-__.disposer(function() {
-	log.info(`Stopping ${app.name} ${app.version} isMaster:${libCluster.isMaster()} config:${JSON.stringify(app.config, null, 4)}`);
-});
+//const libCluster = require '/lib/xp/cluster';
+//log.info(`Starting ${app.name} ${app.version} isMaster:${libCluster.isMaster()} config:${JSON.stringify(app.config, null, 4)}`);
+log.info(`Starting ${app.name} ${app.version} config:${JSON.stringify(app.config, null, 4)}`);
+//__.disposer(function() {
+	//log.info(`Stopping ${app.name} ${app.version} isMaster:${libCluster.isMaster()} config:${JSON.stringify(app.config, null, 4)}`);
+	//log.info(`Stopping ${app.name} ${app.version} config:${JSON.stringify(app.config, null, 4)}`);
+//});
